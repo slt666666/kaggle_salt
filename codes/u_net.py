cf669ac6a1ee3f15e87fe56be9cb90b71fa64e6f
slt666666 <toshi6661024@gmail.com>
2018-09-08 08:07:34 +0000

    first commit

---
+++ codes/u_net.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import random
+
+import pandas as pd
+import numpy as np
+# import matplotlib.pyplot as plt
+# plt.style.use('seaborn-white')
+# import seaborn as sns
+# sns.set_style("white")
+
+# %matplotlib inline
+
+# import cv2
+from sklearn.model_selection import train_test_split
+
+from tqdm import tqdm_notebook, tnrange
+from itertools import chain
+from skimage.io import imread, imshow, concatenate_images
+from skimage.transform import resize
+from skimage.morphology import label
+
+from keras.models import Model, load_model
+from keras.layers import Input,Dropout,BatchNormalization,Activation,Add
+from keras.layers.core import Lambda
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.merge import concatenate
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from keras import backend as K
+
+import tensorflow as tf
+
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
